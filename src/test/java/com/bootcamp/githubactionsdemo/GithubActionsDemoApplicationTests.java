7975ff31baf3d44f2919011da35be4983cb54e0f
suyash kumar singh <suyashs@thoughtworks.com>
2023-06-22 17:44:27 +0530

    Remove. dependency of postgres and flyway

--- src/test/java/com/bootcamp/githubactionsdemo/GithubActionsDemoApplicationTests.java
+++ src/test/java/com/bootcamp/githubactionsdemo/GithubActionsDemoApplicationTests.java
@@ -3,6 +3,11 @@ package com.bootcamp.githubactionsdemo;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+@SpringBootTest
 class GithubActionsDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
 
 }
