2cb3b78f01e6f6cb56b1d1c61e49793f50f098b0
suyash kumar singh <suyashs@thoughtworks.com>
2023-06-22 17:39:57 +0530

    Remove. initial empty test

--- src/test/java/com/bootcamp/githubactionsdemo/GithubActionsDemoApplicationTests.java
+++ src/test/java/com/bootcamp/githubactionsdemo/GithubActionsDemoApplicationTests.java
@@ -3,11 +3,6 @@ package com.bootcamp.githubactionsdemo;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class GithubActionsDemoApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
 
 }
